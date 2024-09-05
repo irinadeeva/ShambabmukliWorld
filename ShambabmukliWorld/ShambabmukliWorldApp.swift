21ad7eb9ede86701258df171b4b63b049d1a0593
irinadeeva <deeva97@me.com>
2024-09-05 10:10:30 +0300

    Initial Commit

---
+++ ShambabmukliWorld/ShambabmukliWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShambabmukliWorldApp.swift
+//  ShambabmukliWorld
+//
+//  Created by Irina Deeva on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShambabmukliWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
